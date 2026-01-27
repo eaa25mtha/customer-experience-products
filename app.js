46431cfca85bbabac4fbdce4e96a0e1621cba280
eaa25mtha <eaa25mtha@students.eaaa.dk>
2026-01-27 11:19:02 +0100

    test af dom-manipulation og konsollen

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ document.addEventListener("DOMContentLoaded", initApp);
 function initApp() {
   console.log("App initialized");
 }
+
+const productTitle = "Fjallraven - Foldsack No. 1 Backpack"; //string
+const productPrice = 849; //number
+const inStock = true; //boolean
+
+//typeof bruges til at skrive datatypen i konsollen
+console.log(typeof productTitle);
+console.log(typeof productPrice);
+console.log(typeof inStock);
+
+//Test af dom-manipulation
+const testSection = document.querySelector("#test");
+const html = `
+  <div>
+    <h3>Test: Produkt information</h3>
+    <p><strong>Titel:</strong> ${productTitle}</p>
+    <p><strong>Pris:</strong> ${productPrice} kr</p>
+    <p><strong>På lager:</strong> ${inStock}</p>
+  </div>
+`;
+testSection.insertAdjacentHTML("berforeend", html);
