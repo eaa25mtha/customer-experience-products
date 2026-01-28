c6c6432baa6bd8cd40a2c3adebaa8b1968951e18
eaa25mtha <eaa25mtha@students.eaaa.dk>
2026-01-28 11:54:56 +0100

    tilføjet properties til objekter og testet med console.log

--- app.js
+++ app.js
@@ -16,13 +16,22 @@ const product1 = {
   title: "Fjällraven - Foldsack No. 1 Backpack",
   price: 849,
   inStock: true,
+  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+  description: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+  category: "Outdoor gear",
 };
 
 const product2 = {
   title: "Mens Casual Premium Slim Fit T-Shirts",
   price: 179,
   inStock: true,
+  image:
+    "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+  description: "Slim-fitting style, contrast raglan long sleeve, three-buttons",
+  category: "men's clothing",
 };
 
 console.log(product1);
 console.log(product2);
+console.log(product1.title);
+console.log(product1.title + "koster" + product1.price + "kr");
