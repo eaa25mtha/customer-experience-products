eef75cd9bb71867918e18591d4bb77526254e06c
eaa25mtha <eaa25mtha@students.eaaa.dk>
2026-01-28 11:23:09 +0100

    vi det billigste produkt på siden ved brug af en if-sætning

--- app.js
+++ app.js
@@ -12,17 +12,19 @@ function initApp() {
 // ======== TEST SEKTION ===========
 //produkt 1
 const productTitle = "Fjallraven - Foldsack No. 1 Backpack"; //string
-const productPrice = 849; //number
+const productPrice = 800; //number
 const inStock = true; //boolean
 
-//typeof bruges til at skrive datatypen i konsollen
-console.log(typeof productTitle);
-console.log(typeof productPrice);
-console.log(typeof inStock);
+//produkt 2
+const productTitle2 = "Mens Casual Premium Slim Fit T-Shirts";
+const productPrice2 = 200;
+const inStock2 = false;
 
-//Test af dom-manipulation, indsæt i html
-const testSection = document.querySelector("#test");
-const html = `
+//dom-manipulation
+if (productPrice < productPrice2) {
+  //hvis prisen på produkt 1 er mindst, skal den vises, ellers skal produkt 2 vises på siden
+  const testSection = document.querySelector("#test");
+  const html = `
   <div>
     <h3>Test: Produkt information 1</h3>
     <p><strong>Titel:</strong> ${productTitle}</p>
@@ -30,21 +32,10 @@ const html = `
     <p><strong>På lager:</strong> ${inStock}</p>
   </div>
 `;
-testSection.insertAdjacentHTML("beforeend", html);
-
-if (inStock) {
-  console.log(`${productTitle} er på lager!`);
+  testSection.insertAdjacentHTML("beforeend", html);
 } else {
-  console.log(`${productTitle} er desværre ikke på lager!`);
-}
-
-//produkt 2
-const productTitle2 = "Mens Casual Premium Slim Fit T-Shirts";
-const productPrice2 = 200;
-const inStock2 = false;
-
-const testSection2 = document.querySelector("#test");
-const html2 = `
+  const testSection2 = document.querySelector("#test");
+  const html2 = `
   <div>
     <h3>Test 2: Produkt information 2</h3>
     <p><strong>Titel:</strong> ${productTitle2}</p>
@@ -52,7 +43,15 @@ const html2 = `
     <p><strong>På lager</strong> ${inStock2}</p>
   </div>
 `;
-testSection2.insertAdjacentHTML("beforeend", html2);
+  testSection2.insertAdjacentHTML("beforeend", html2);
+}
+
+//if-sætning der tester om produktet er på lager og udskriver til konsollen
+if (inStock) {
+  console.log(`${productTitle} er på lager!`);
+} else {
+  console.log(`${productTitle} er desværre ikke på lager!`);
+}
 
 if (inStock2) {
   console.log(`${productTitle2} er på lager!`);
@@ -60,20 +59,9 @@ if (inStock2) {
   console.log(`${productTitle2} er desværre ikke på lager!`);
 }
 
-//sammenligning af priser med if-sætninger
+//sammenligning af priser
 if (productPrice > productPrice2) {
   console.log(`${productTitle} er dyrest og koster ${productPrice} kr`);
 } else {
   console.log(`${productTitle2} er dyrest og koster ${productPrice2} kr`);
-}
-
-//vis billigste produkt med if-sætning
-if (productPrice > productPrice2) {
-  console.log(
-    `${productTitle} er billigst, koster ${productPrice} kr og er på lager = ${inStock}`, //hvordan skriver man det uden at den viser true i konsollen?
-  );
-} else {
-  console.log(
-    `${productTitle2} er billigst, koster ${productPrice2} kr og er på lager = ${inStock2}`,
-  );
 }
