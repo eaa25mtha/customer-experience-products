e0fec86346e15ef2eb40c52cc14333fef931b84f
eaa25mtha <eaa25mtha@students.eaaa.dk>
2026-01-29 17:37:09 +0100

    displayProduct funktion product.js

--- product.js
+++ product.js
@@ -9,4 +9,36 @@ async function initProduct() {
   const urlParams = new URLSearchParams(window.location.search); //returnerer ?id=1
   const id = urlParams.get("id"); //parser parametrene
   console.log("product ID:", id); //henter værdien af id parameteren
+
+  //hent alle produkter
+  const products = await getAllProducts();
+
+  //find det specifikke produkt
+  const product = products.find((p) => p.id == id);
+
+  if (product) {
+    displayProduct(product);
+  } else {
+    document.querySelector("#product").innerHTML =
+      "<p>produkt ikke fundet!</p>";
+  }
+}
+
+//vis produktet
+function displayProduct(product) {
+  const html = /*html*/ `
+    <div class="product-detail-container">
+        <div class="product-image-large">
+            <img src"${product.image}" alt="${product.title}"/>
+        </div>
+        <div class="product-details">
+            <h1>${product.title}</h1>
+            <p class="category">Kategori: ${product.category}</p>
+            <p class="price-large"> $${product.price}</p>
+            <p>${product.description}</p>
+        </div>
+        </div>
+    `;
+
+  document.querySelector("#product").innerHTML = html;
 }
